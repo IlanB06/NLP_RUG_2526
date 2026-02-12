40ed272a56ac2715cc2fb95e9df2cad5482492f7
Bertran <bertran.vd.giessen@hotmail.com>
2026-02-12 12:34:30 +0100

    implemented data loader with the required train/dev/test split

---
+++ data/load_data.py
@@ -0,0 +1,18 @@
+from datasets import load_dataset
+
+SEED = 42
+
+def load_ag_news():
+    dataset = load_dataset("ag_news")
+
+    train_dev = dataset["train"].train_test_split(
+        test_size=0.1,
+        seed=SEED
+    )
+
+    # Original train/test split is used. Additionally, 10% of all data is taken from the train set as the dev set.
+    train_dataset = train_dev["train"]
+    dev_dataset = train_dev["test"]
+    test_dataset = dataset["test"]
+
+    return train_dataset, dev_dataset, test_dataset
