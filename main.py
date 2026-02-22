8c98bd28f69f0144d8f1dddd4ae65f8c31eea910
Mathis Sibon <mathissibon@Mathiss-MacBook-Pro.local>
2026-02-22 20:37:28 +0100

    small changes to error section

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from src.linear_models.eval_linear_models import evaluate_linear_models
-
+from src.linear_models.error_analysis import collect_misclassified_svm
 
 def main():
     evaluate_linear_models()
